c2e974c169176912db6b8385f2b2dac20b754774
Emmanuel (Maxzeno) <mmnlchidera@gmail.com>
2022-12-26 17:53:06 +0100

    staticfiles

--- dphone/wsgi.py
+++ dphone/wsgi.py
@@ -8,13 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dphone.settings')
-
-# if settings._DEPLOY:
-# 	os.system('python manage.py migrate')
-# 	os.system('python manage.py collectstatic')
 
 application = get_wsgi_application()
